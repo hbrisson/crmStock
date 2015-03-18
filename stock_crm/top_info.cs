f1445dd214b03a5ed42df5bbc614eaf30c331fea
RodrigueT <rodriguetillard@gmail.com>
2015-03-18 18:19:29 +0100

    no message

--- stock_crm/top_info.cs
+++ stock_crm/top_info.cs
@@ -16,7 +16,7 @@ namespace stock_crm
         private string query_user = " SELECT u.nom, u.prenom, (SELECT COUNT(*) FROM usertiers ut WHERE ut.idusers = u.id) AS Nbtiers, COUNT(c.id) AS NbCommande, SUM(c.montant) AS Montant_Total FROM user u, usertiers ut, commande c, tiers t WHERE u.id = ut.idusers AND ut.idtiers = t.id AND t.id = c.idtiers  GROUP BY ut.idusers";
         private string query_cmd = "Select t.nom as NomTiers,c.type as Type,c.etat as Etat,c.date as Date,p.nom as NomProduit,p.prix as Prix,cp.quantite as Quantite,c.montant as Montant from commande c ,usertiers ut ,commandeproduit cp ,produit p ,tiers t where c.idtiers = t.id and t.id = ut.idtiers and c.id = cp.idcommande and cp.idproduit = p.id AND c.etat != 'refusée' AND c.etat != 'validée' AND c.etat != 'annulée'";
         private string query_top_client = "SELECT t.id, t.nom, t.secteur, t.type, SUM(c.montant) AS Somme FROM commande c, tiers t WHERE c.idtiers = t.id GROUP BY idtiers ORDER BY Somme DESC";
-        private string query_top_produit = "SELECT p.id, p.nom, p.quantite, SUM(cp.idproduit) AS NbCommandes, cp.idcommande, SUM(c.montant) AS MontantTotal FROM produit p, commandeproduit cp, commande c WHERE p.id = cp.idproduit AND c.id = cp.idcommande GROUP BY idproduit ORDER BY NbCommandes DESC";
+        private string query_top_produit = "SELECT p.id, p.nom, SUM(cp.quantite) AS quantite, SUM(cp.idproduit) AS NbCommandes, cp.idcommande, SUM(c.montant) AS MontantTotal FROM produit p, commandeproduit cp, commande c WHERE p.id = cp.idproduit AND c.id = cp.idcommande GROUP BY idproduit ORDER BY NbCommandes DESC";
 
         public top_info()
         {
@@ -53,23 +53,27 @@ namespace stock_crm
             CultureInfo francais = CultureInfo.GetCultureInfo("fr-FR");
 
             //On initialise la date d'aujourd'hui ainsi que la date de comparaison
-            DateTime date = DateTime.Today;
-            var str_date = date.ToShortDateString();
+           // string date = DateTime.Today.ToString("yyyy-MM-dd H:mm:ss");
+            DateTime date = DateTime.Now;
+            //var str_date = date.ToShortDateString();
             DateTime intervalle = date.AddMonths(-1);
-            var str_intervalle = intervalle.ToShortDateString();
+            //var str_intervalle = intervalle.ToShortDateString();
+            string str_date = date.ToString("yyyy-MM-dd H:mm:ss");
+            string str_date_inter = intervalle.ToString("yyyy-MM-dd H:mm:ss");
+
 
             //On met la date au format nécéssaire AAAA/MM/DD
-            string dateEnAnglais = DateTime.Parse(str_date, francais).ToString(anglais);
-            string dateEnAnglais_intervalle = DateTime.Parse(str_intervalle, francais).ToString(anglais);
+            //string dateEnAnglais = DateTime.Parse(str_date, francais).ToString(anglais);
+            //string dateEnAnglais_intervalle = DateTime.Parse(str_intervalle, francais).ToString(anglais);
 
             //On ne garde que la date
-            string[] Words = dateEnAnglais.Split(new char[] { ' ' });
-            dateEnAnglais = Words[0];
-            string[] Words_inter = dateEnAnglais_intervalle.Split(new char[] { ' ' });
-            dateEnAnglais_intervalle = Words_inter[0];
+            //string[] Words = dateEnAnglais.Split(new char[] { ' ' });
+            //dateEnAnglais = Words[0];
+            //string[] Words_inter = dateEnAnglais_intervalle.Split(new char[] { ' ' });
+            //dateEnAnglais_intervalle = Words_inter[0];
 
             //On effectue la requete et on remplit le tableau
-            string query_lastcmd = "Select t.nom as NomTiers,c.type as Type,c.etat as Etat,c.date as Date,p.nom as NomProduit,p.prix as Prix,cp.quantite as Quantite,c.montant as Montant from commande c ,usertiers ut ,commandeproduit cp ,produit p ,tiers t where c.idtiers = t.id and t.id = ut.idtiers and c.id = cp.idcommande and cp.idproduit = p.id AND c.date BETWEEN '" + dateEnAnglais_intervalle + "' AND '" + dateEnAnglais + "'";
+            string query_lastcmd = "Select t.nom as NomTiers,c.type as Type,c.etat as Etat,c.date as Date,p.nom as NomProduit,p.prix as Prix,cp.quantite as Quantite,c.montant as Montant from commande c ,usertiers ut ,commandeproduit cp ,produit p ,tiers t where c.idtiers = t.id and t.id = ut.idtiers and c.id = cp.idcommande and cp.idproduit = p.id AND c.date <= '" + str_date + "' AND c.date >='" + str_date_inter + "'";
             Toolbox.loadGridView(dataGridView1, accueilConnect.connection, null, query_lastcmd);
         }
 
